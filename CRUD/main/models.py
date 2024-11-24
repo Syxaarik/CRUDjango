6910ef9050ac5018d002a6580502e089bb4acf8c
Artem <artemmarkovskiy2009@gmail.com>
2024-11-24 17:36:20 +0300

    Реализация CRUD системы

--- CRUD/main/models.py
+++ CRUD/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    objects = None
+    DoesNotExist = None
+    name = models.CharField(max_length=20)
+    second_name = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.second_name
